bf2a65191e14ba23a77714d9d218b22bcc6a0e74
Eddie Allard <eddieallard27@hotmail.com>
2019-11-11 01:27:01 -0500

    added link to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Global variables below this line
+// Array of Word Options (all lowercase).
+var wordsList = ["California", "Florida", "New York", "New Jersey", "Vermont", "Arizona",
+  "Texas", "New Mexico", "Louisiana", "Georgia", "North Carolina", "Virginia", "Delaware", "South Carolina"];
+
+  // This will be the computer choice
+  var chosenWord = "";
+
+  // Individual letters to be stored in the array
+  var lettersInChosenWord = [];
+
+  // Number of blanks to be shown
+  var numBlanks = 0;
+
+  
